7d78fb9c18e234af78bef844d5c5fc29aa944b97
Davi Reis <reis.davi11010@gmail.com>
2022-08-06 16:10:38 -0300

    feat: setted main content layout and files

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,5 +3,6 @@ import "@fontsource/nunito/500.css";
 import "@fontsource/nunito/600.css";
 
 import "../styles/general.css";
-import "../styles/header.css";
 import "../styles/components.css";
+import "../styles/header.css";
+import "../styles/main.css";
