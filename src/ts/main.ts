bc4dac51a0d991a0c1280f18771169bee0a2e856
Davi Reis <reis.davi11010@gmail.com>
2022-08-06 17:10:04 -0300

    feat: implemented main content styles

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,6 +1,7 @@
 import "@fontsource/nunito";
 import "@fontsource/nunito/500.css";
 import "@fontsource/nunito/600.css";
+import "@fontsource/nunito/700.css";
 
 import "../styles/general.css";
 import "../styles/components.css";
