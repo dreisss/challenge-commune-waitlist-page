aa111525ff8324815c195eba3a2bec45a8db3a70
Davi Reis <reis.davi11010@gmail.com>
2022-08-06 11:34:25 -0300

    feat: added and applyed components styles file

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,5 +1,7 @@
 import "@fontsource/nunito";
 import "@fontsource/nunito/500.css";
 import "@fontsource/nunito/600.css";
+
 import "../styles/general.css";
 import "../styles/header.css";
+import "../styles/components.css";
